4e80e9c182063b82eb9648e41703fe42270b4639
DavidAlejandroHenriquez <da.henriquez10@uniandes.edu.co>
2017-04-26 14:46:11 -0500

    5

--- shock.c
+++ shock.c
@@ -20,21 +20,21 @@ int main(void){
 
   for(int i = 0; i<(N+1); i++){
     if(i<(N/2+1)){
-      w_1[i]  = 1.0;
+      w_1[i]  = 1.0/0.1;
       w_2[i]  = 0.0;
-      w_3[i]  = energy(1.0,1.0,0.0);
+      w_3[i]  = energy(1.0/(1.4*0.1),1.0/0.1,0.0);
     }
     else{
-      w_1[i] = 0.1;
+      w_1[i] = 1.0;
       w_2[i]  = 0.0;
-      w_3[i]  = energy(0.1,0.1,0.0);
+      w_3[i]  = energy(1.0/1.4,1.0,0.0);
     }
   }
 
   Lax_Wendroff(w_1,w_2,w_3);
 
   for(int i = 0; i<(N+1); i++){
-    printf("%f\t%f\t%f\n",w_1[i],((2.0/3)*(w_3[i]-(w_2[i]*w_2[i])/(2*w_1[i]))),w_2[i]/w_1[i]);
+    printf("%f\t%f\t%f\n",w_1[i],((2.0/5)*(w_3[i]-(w_2[i]*w_2[i])/(2*w_1[i]))),w_2[i]/w_1[i]);
   }
 
   free(w_1);
@@ -44,17 +44,17 @@ int main(void){
 }
 
 float energy(float pressure, float density, float velocity){
-  return ((3.0/2)*pressure + (density/2.0)*velocity*velocity);
+  return ((5.0/2)*pressure + (density/2.0)*velocity*velocity);
 }
 
 float F1(float w1, float w2, float w3){
   return w2;
 }
 float F2(float w1, float w2, float w3){
-  return ((w2*w2)/w1) + (2.0/3)*(w3 - ((w2*w2)/(2.0*w1)));
+  return ((w2*w2)/w1) + (2.0/5)*(w3 - ((w2*w2)/(2.0*w1)));
 }    
 float F3(float w1, float w2, float w3){
-  return (w3 + (2.0/3)*(w3 - ((w2*w2)/(2.0*w1))))*(w2/w1);
+  return (w3 + (2.0/5)*(w3 - ((w2*w2)/(2.0*w1))))*(w2/w1);
 }
 float max_calculator(float *arr){
   float max = arr[0];
@@ -68,7 +68,7 @@ float max_calculator(float *arr){
 float time_step_calculator(float cfl,float* w1, float* w2, float* w3,float dx){
   float w_dt[N]; 
   for(int i = 0; i<N; i++){
-    float t = (float) fabs(w2[i]/w1[i]) + pow((10.0/9)*((w3[i]/w1[i]) - (w2[i]*w2[i])/(2*w1[i]*w1[i])),0.5);
+    float t = (float) fabs(w2[i]/w1[i]) + pow((14.0/25)*((w3[i]/w1[i]) - (w2[i]*w2[i])/(2*w1[i]*w1[i])),0.5);
     w_dt[i] = t; 
   }
   return cfl*(dx/max_calculator(w_dt));
@@ -86,7 +86,7 @@ void Lax_Wendroff(float* w1, float* w2, float* w3){
   float w_medios_3_p = 0;
   
   int n = 1;
-  while( n < 6150 ){
+  while( n < 5600 ){
     
     float dt = time_step_calculator(0.1,w1,w2,w3,dx); 
 
